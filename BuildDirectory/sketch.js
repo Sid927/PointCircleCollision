999f11c1ae18b4b814a75adc383a3586418c90da
Sid927 <54078962+Sid927@users.noreply.github.com>
2020-07-17 11:35:03 -0700

    Add files via upload

---
+++ BuildDirectory/sketch.js
@@ -0,0 +1,28 @@
+let pr = 0;
+let cx = 300;
+let cy = 400;
+let cr = 50
+let hit = false;
+
+const checkCollisionPointCircle = (px, py, pr, cx, cy, cr) => {
+  let d = Math.pow(Math.pow((px - cx), 2) + Math.pow((py - cy), 2), 1 / 2);
+  if (pr + cr >= d) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function setup() {
+  createCanvas(750, 750);
+}
+
+function draw() {
+  hit = false;
+  background(255);
+  circle(cx, cy, cr * 2);
+  point(mouseX, mouseY);
+  hit = checkCollisionPointCircle(mouseX, mouseY, pr, cx, cy, cr);
+  stroke( (hit) ? color("red"): 0);
+  console.log('Colliding: ' + hit)
+}
